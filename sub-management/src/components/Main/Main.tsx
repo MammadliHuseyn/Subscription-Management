b33e8bc8f13ddb09ca0a7214e7e5c67425ba1c76
FuadHashimov91 <fuadbh@code.edu.az>
2021-06-11 17:42:14 +0400

    Add Search Component

--- sub-management/src/components/Main/Main.tsx
+++ sub-management/src/components/Main/Main.tsx
@@ -1,3 +1,4 @@
+import  Search from "./Search";
 import Navbar from "../Navbar/Navbar";
 import Pagination from "./Pagination";
 
@@ -9,47 +10,7 @@ const Main = () => {
           <div id="content">
             <Navbar />
             <div className="container">
-              <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                <form
-                  className="
-                    d-sm-inline-block
-                    form-inline
-                    mr-auto
-                    ml-md-3
-                    my-2 my-md-0
-                    mw-100
-                    navbar-search
-                    w-75
-                    border
-                    rounded
-                   "
-                >
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      className="form-control bg-light border-0 small"
-                      placeholder="Search for..."
-                      aria-label="Search"
-                      aria-describedby="basic-addon2"
-                    />
-                    <div className="input-group-append">
-                      <button className="btn btn-primary" type="button">
-                        <i className="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-                <a
-                  href="#"
-                  className="
-                  d-sm-inline-block
-                  btn btn-sm btn-primary
-                  shadow-sm
-                "
-                >
-                  <i className="fa-sm text-white-50"></i> Create Subscription
-                </a>
-              </div>
+              <Search/>
               <div className="row">
                 <div className="col-xl-3 col-md-6 mb-4 " id="card">
                   <div className="card" style={{ width: "18rem;" }}>
