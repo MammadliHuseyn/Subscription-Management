3fed7b4d54cfafd700e8e46287729f7a3c5f0169
MammadliHuseyn <Huseynym@code.edu.az>
2021-06-11 11:40:01 +0400

    Interface types created for user & subscription

--- sub-management/src/types/index.ts
+++ sub-management/src/types/index.ts
@@ -1,1 +1,43 @@
 import React from 'react';
+
+export interface IUserLogin {
+    username: string;
+    password: string;
+}
+export interface IUserRegister {
+    username: string;
+    password: string;
+    active: Boolean;
+    name: string;
+    surname: string;
+    email: string;
+}
+export interface IStoredUser {
+    id: string;
+    username: string;
+    email: string;
+    name: string;
+    surname: string;
+}
+interface IGroup {
+    id: number;
+    value: string;
+    type: string;
+}
+interface IPrice {
+    id: number;
+    amount: string;
+    currency: string;
+}
+interface ICategory {
+    id: number;
+    name: string;
+}
+export interface ISubscription {
+    id: number;
+    name: string;
+    price: IPrice;
+    userId: number;
+    group: IGroup;
+    category: ICategory;
+}
