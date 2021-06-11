a285b01acbe5064ab4865f1813b93f5346618750
MammadliHuseyn <Huseynym@code.edu.az>
2021-06-11 13:23:21 +0400

    Store provided to App component

--- sub-management/src/store/store.ts
+++ sub-management/src/store/store.ts
@@ -1,1 +1,8 @@
-import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { rootReducer } from './rootReducer';
+
+const middleWares = applyMiddleware(logger, thunk);
+
+export const store = createStore(rootReducer, middleWares);
