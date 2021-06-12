85d00d19f4de0638e00c16f949c96c9a4cecea2a
MammadliHuseyn <Huseynym@code.edu.az>
2021-06-12 18:04:56 +0400

    Card css updated

--- sub-management/src/components/Navbar/UserCard.tsx
+++ sub-management/src/components/Navbar/UserCard.tsx
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import { STORAGE_ACTIONS, userActionsWithStore } from '../../store/user/storage';
 import { IUser } from '../../types';
 
+
+
 export const UserCard = () => {
     const user: IUser = React.useMemo(() => {
         return userActionsWithStore(undefined, STORAGE_ACTIONS.GET_USER_FROM_STORAGE);
@@ -15,7 +17,7 @@ export const UserCard = () => {
                 <Typography color="textSecondary" gutterBottom>
                     {user.name} {user.surname}
                 </Typography>
-                <Typography variant="h6" component="h6">
+                <Typography variant="h6" component="h6" style={{display:"flex", fontWeight: "bold", fontSize: "14px"}}>
                     Email: {user.email}
                 </Typography>
             </CardContent>
